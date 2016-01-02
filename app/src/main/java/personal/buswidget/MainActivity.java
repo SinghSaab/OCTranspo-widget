82a1eea2df21d1a84bd41598af160387ae2bbf83
TaranjeetSingh <singh.taranjeet16@gmail.com>
2016-01-01 23:48:36 -0500

    Static data working fine

--- app/src/main/java/personal/buswidget/MainActivity.java
+++ app/src/main/java/personal/buswidget/MainActivity.java
@@ -1,44 +1,28 @@
 package personal.buswidget;
 
-import android.app.Application;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.RemoteViews;
-import android.widget.Toast;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-
-import java.io.IOException;
 
 public class MainActivity extends AppWidgetProvider {
-    String title;
-    String reachURL = "http://www.ocbustracker.com/BusData.aspx?stopno=6441&busnum0=85&direction0=Eastbound&agency=OC";
+    String reachURL = "http://www.google.com";
+    RemoteViews remoteViews;
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         final int count = appWidgetIds.length;
-        super.onUpdate(context, appWidgetManager, appWidgetIds);
-
-        Log.d("OnUpdate", "Before ASyncExecute");
-
-//        new getTimings().execute(reachURL);
-
-        Log.d("OnUpdate", "After ASyncExecute");
-
-
         for (int i = 0; i < count; i++) {
             int widgetId = appWidgetIds[i];
-            Log.d("", "Getting Remote View");
-            RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
+            remoteViews = new RemoteViews(context.getPackageName(),
                     R.layout.widget_layout);
-            remoteViews.setTextViewText(R.id.textView_Char, "hello");
-
+            remoteViews.setTextViewText(R.id.textView_Char, "Default Text");
+            Log.d("","Executing Async");
+            new getTimings(remoteViews).execute(reachURL);
+//            Update widget Intent
             Intent intent = new Intent(context, MainActivity.class);
             intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
             intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
@@ -48,24 +32,7 @@ public class MainActivity extends AppWidgetProvider {
             remoteViews.setOnClickPendingIntent(R.id.textView_No, pendingIntent);
             appWidgetManager.updateAppWidget(widgetId, remoteViews);
         }
-    }
 
-    public class getTimings extends AsyncTask<String, Void, String> {
-        @Override
-        protected String doInBackground(String... params) {
-            try {
-                Document document = Jsoup.connect(params[0]).get();
-                title = document.title();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return title;
-        }
-        @Override
-        protected void onPostExecute(String string) {
-            super.onPostExecute(title);
-
-        }
     }
 }
 
--- app/src/main/java/personal/buswidget/getTimings.java
+++ app/src/main/java/personal/buswidget/getTimings.java
@@ -1,7 +1,35 @@
 package personal.buswidget;
 
+import android.os.AsyncTask;
+import android.widget.RemoteViews;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 /**
  * Created by Administrator on 2016-01-01.
  */
-public class getTimings {
+public class getTimings extends AsyncTask<String, Void, String> {
+    String title;
+    private RemoteViews remoteView;
+    public getTimings(RemoteViews remoteViews) {
+        this.remoteView = remoteViews;
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        try {
+            Document document = Jsoup.connect(params[0]).get();
+            title = "Document.title()";
+        } catch (Exception e) {
+            title = "Error Fetching";
+            e.printStackTrace();
+        }
+        return title;
+    }
+
+    @Override
+    protected void onPostExecute(String string) {
+        remoteView.setTextViewText(R.id.textView_Char, string);
+    }
 }
