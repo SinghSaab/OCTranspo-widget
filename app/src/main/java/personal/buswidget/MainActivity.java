05dd9d74101fa8f2142647ab3bf1a56b5defa0ae
TaranjeetSingh <singh.taranjeet16@gmail.com>
2016-01-02 21:43:14 -0500

    Static data working fine

--- app/src/main/java/personal/buswidget/MainActivity.java
+++ app/src/main/java/personal/buswidget/MainActivity.java
@@ -5,65 +5,31 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
-import android.os.AsyncTask;
+import android.util.Log;
 import android.widget.RemoteViews;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-
 public class MainActivity extends AppWidgetProvider {
-    String reachURL = "http://www.google.com";
+    String fetchURL = "http://www.google.com";
     RemoteViews remoteViews;
-    int widgetId;
+    protected int widgetId, counter = 0;
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
         final int count = appWidgetIds.length;
         for (int i = 0; i < count; i++) {
             widgetId = appWidgetIds[i];
-            remoteViews = new RemoteViews(context.getPackageName(),
-                    R.layout.widget_layout);
-            new getTimings(remoteViews).execute(reachURL);
-//            remoteViews.setTextViewText(R.id.textView_Char, returnTitle);
-//            Update widget Intent
+            counter = counter + 3;
+            Log.d("Counter Value:", String.valueOf(counter));
+            remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+            remoteViews.setTextViewText(R.id.textView_Char, "Counter=" + String.valueOf(counter));
             Intent intent = new Intent(context, MainActivity.class);
             intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
             intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
-//            To request a manual update when the update button is clicked, we use a PendingIntent.
-            PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
-                    0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
             remoteViews.setOnClickPendingIntent(R.id.textView_No, pendingIntent);
             appWidgetManager.updateAppWidget(widgetId, remoteViews);
         }
-
-    }
-
-
-    public class getTimings extends AsyncTask<String, Void, String> {
-        public String title = "Default Text";
-        private RemoteViews remoteView;
-
-        public getTimings(RemoteViews remoteViews) {
-            this.remoteView = remoteViews;
-        }
-
-        @Override
-        protected String doInBackground(String... params) {
-            try {
-                Document document = Jsoup.connect(params[0]).get();
-                title = "Document.title()";
-            } catch (Exception e) {
-                title = "Error Fetching";
-                e.printStackTrace();
-            }
-            return title;
-        }
-
-        @Override
-        protected void onPostExecute(String string) {
-            remoteView.setTextViewText(R.id.textView_Char, string);
-        }
-
     }
 }
 
