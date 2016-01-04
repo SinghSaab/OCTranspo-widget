428f40ce6749c408c7eddc26c15890b47b1ff9f7
TaranjeetSingh <singh.taranjeet16@gmail.com>
2016-01-03 21:57:09 -0500

    Jsoup scraped. It can't parse javascript elements. Using the OCTranspo API, which will provide XML data for search query

--- app/src/main/java/personal/buswidget/AsyncClass.java
+++ app/src/main/java/personal/buswidget/AsyncClass.java
@@ -1,27 +1,76 @@
 package personal.buswidget;
 
+import android.app.Application;
+import android.content.Context;
 import android.os.AsyncTask;
+import android.util.Log;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 /**
  * Created by Administrator on 2016-01-02.
  */
-public class AsyncClass extends AsyncTask<String, Void, String> {
+
+public class AsyncClass extends AsyncTask<Void, Void, String> {
     public AsyncResponse delegate = null;
-    String title;
+    String appID = "*** Enter your appID from OCTranspo account ***";
+    String appKey = "*** Enter your appKey from OCTranspo account ***";
+    String stopNo = "6410";
+    String routeNo = "85";
 
     @Override
-    protected String doInBackground(String... params) {
+    protected String doInBackground(Void... params) {
+        String routeSummary = "https://api.octranspo1.com/v1.2/GetRouteSummaryForStop";
+        String routeSummaryParam = "appID=" + appID + "&apiKey=" + appKey + "&stopNo=" + stopNo;
+        String nextTrip = "https://api.octranspo1.com/v1.2/GetNextTripsForStop";
+        String nextTripParam = "appID=" + appID + "&apiKey=" + appKey + "routeNo" + routeNo + "&stopNo=" + stopNo;
+
         try {
-            Document document = Jsoup.connect(params[0]).get();
-            title = document.title();
+            Log.d("", "Crafting URL...");
+            URL url = new URL(routeSummary);
+            Log.d("Crafting URL", "Done");
+            Log.d("", "Opening HttpsConnection String...");
+            HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("POST");
+            urlConnection.setDoOutput(true);
+            DataOutputStream dStream = new DataOutputStream(urlConnection.getOutputStream());
+            dStream.writeBytes(routeSummaryParam);
+            dStream.flush();
+            dStream.close();
+            int responseCode = urlConnection.getResponseCode();
+            final StringBuilder output = new StringBuilder("Request URL " + url);
+            output.append(System.getProperty("line.separator") + "Request Parameters " + routeSummaryParam);
+            output.append(System.getProperty("line.separator") + "Response Code " + responseCode);
+            Log.d("Opening HttpsConnection", "Done");
+            try {
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                StringBuilder stringBuilder = new StringBuilder();
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(line).append("\n");
+                }
+                bufferedReader.close();
+                Log.d("OUPUT:::::", stringBuilder.toString());
+                return "OUTPUT";
+            } catch (Exception e) {
+                e.printStackTrace();
+                return "Exception Occured while StringBuilder!";
+            } finally {
+                Log.d("", "Closing HttpsConnection...");
+                urlConnection.disconnect();
+                Log.d("Closing HttpsConnection", "Done");
+            }
         } catch (Exception e) {
-            title = "Error Fetching";
             e.printStackTrace();
+            return "Exception Occured while HttpsURLConnection!";
         }
-        return title;
     }
 
     @Override
--- app/src/main/java/personal/buswidget/MainActivity.java
+++ app/src/main/java/personal/buswidget/MainActivity.java
@@ -5,40 +5,35 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 import android.widget.RemoteViews;
 
 public class MainActivity extends AppWidgetProvider implements AsyncResponse {
-    public static int counter =0;
-    static String title;
+    public static String asyncOutput;
     RemoteViews remoteViews;
     int widgetId;
+
+//    public static int counter =0;
 //    Static only makes one copy for counter variable and hence broadcast receiver would be changing one variable only
 //    Else, it will keep the value reserving back to zero whenever onUpdate() would be called
-
 //    A BroadcastReceiver object is only valid for the duration of the call to onReceive(Context, Intent).
 //    Once your code returns from this function, the system considers the object to be finished and no longer active
 //    Create instance of the AsyncTask class
+
     AsyncClass getResults = new AsyncClass();
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         super.onUpdate(context, appWidgetManager, appWidgetIds);
-
         //this to set delegate/listener back to this class
         getResults.delegate = this;
-
         //execute the async task
         getResults.execute();
-
 //        Update procedure for the widget
         final int count = appWidgetIds.length;
-        counter +=3 ;
         for (int i = 0; i < count; i++) {
             widgetId = appWidgetIds[i];
-            Log.d("Counter Value:", String.valueOf(counter));
             remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
-            remoteViews.setTextViewText(R.id.textView_Char, "Counter=" + String.valueOf(counter)+"\n"+title);
+            remoteViews.setTextViewText(R.id.textView_Char, asyncOutput);
 
             Intent intent = new Intent(context, MainActivity.class);
             intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
@@ -46,15 +41,13 @@ public class MainActivity extends AppWidgetProvider implements AsyncResponse {
 
             PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
             remoteViews.setOnClickPendingIntent(R.id.textView_No, pendingIntent);
-
             appWidgetManager.updateAppWidget(widgetId, remoteViews);
         }
     }
 
     public void processFinish(String output) {
-        //Here you will receive the result fired from AsyncClass
-        //of onPostExecute(result) method.
-        title = output;
+        //Here you will receive the result fired from AsyncClass of onPostExecute(result) method.
+        asyncOutput = output;
     }
 }
 
